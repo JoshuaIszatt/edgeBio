67dd5bcd66003c33a0dff0a025ce790713e74f0e
josh <joshiszatt@gmail.com>
2025-03-22 03:20:53 +0800

    fix: update NotFound component message for clarity

--- frontend/app/not-found.jsx
+++ frontend/app/not-found.jsx
@@ -3,7 +3,7 @@ import Link from "next/link"
 export default function NotFound() {
   return (
     <main className='text-center'>
-        <h2 className='text-3x1'>Error: we cannot retreive your files metadata</h2>
+        <h2 className='text-3x1'>Error: page does not exist</h2>
         <p>Head back to the <Link href='/'>ABOUT</Link> page</p>
     </main>
   )
