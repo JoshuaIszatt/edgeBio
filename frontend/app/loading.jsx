44f7cba70b1a9a354953768ef15f5085bc6c2d58
josh <joshiszatt@gmail.com>
2025-03-22 03:08:59 +0800

    feat: add Loading and NotFound components for improved user experience

---
+++ frontend/app/loading.jsx
@@ -0,0 +1,8 @@
+export default function Loading() {
+  return (
+    <main className="text-center">
+        <h2 className="text-primary">Loading...</h2>
+        <p>Put on a cup of tea or something</p>
+    </main>
+  )
+}
---
+++ frontend/app/not-found.jsx
@@ -0,0 +1,10 @@
+import Link from "next/link"
+
+export default function NotFound() {
+  return (
+    <main className='text-center'>
+        <h2 className='text-3x1'>Error: we cannot retreive your files metadata</h2>
+        <p>Head back to the <Link href='/'>ABOUT</Link> page</p>
+    </main>
+  )
+}
