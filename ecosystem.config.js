9b4bef61a67f8276afa8e0a4a7a24c8513515e11
josh <joshiszatt@gmail.com>
2025-03-24 04:37:42 +0800

    feat: add logging configuration for backend, frontend, and test pipeline

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -14,6 +14,8 @@ module.exports = {
                 UPLOAD_PATH: 'filesystem/',
                 NODE_ENV: 'development',
             },
+            output: "../logs/backend.log",
+            error_file: "../logs/backend-error.log"
         },
         {
             name: 'frontend',
@@ -21,6 +23,18 @@ module.exports = {
             args: 'run dev',
             cwd: './frontend',
             watch: true,
+            output: "../logs/frontend.log",
+            error_file: "../logs/frontend-error.log"
+        },
+        {
+            name: 'test_pipeline',
+            script: 'python3',
+            interpeter_args: '-u',
+            args: 'main.py',
+            cwd: './pipelines/test/app',
+            watch: true,
+            output: "../../../logs/pipelines.log",
+            error_file: "../../../logs/pipelines-error.log"
         }
     ],
 };
