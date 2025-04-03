ff8b5766ff46bd4bde2b6cd7a1b37c8f368b1e06
hermes <joshiszatt@gmail.com>
2025-04-03 10:00:49 +0800

    feat: enhance clear script to remove temporary files and prompt for filesystem clearance

--- clear.sh
+++ clear.sh
@@ -4,3 +4,23 @@
 echo "Clearing log files:"
 ls logs/*.log
 rm logs/*.log
+
+echo "Clearing temporary file:"
+ls ./pipelines/pyTEST/app/message_log.txt
+rm ./pipelines/pyTEST/app/message_log.txt
+
+# Ask to remove (Y/N) filesystem from backend
+while true; do
+    echo "Do you want to remove the filesystem from backend? (Y/N)"
+    read -r answer
+    if [[ $answer == "Y" || $answer == "y" ]]; then
+        echo "Clearing filesystem:"
+        rm -rf ./backend/filesystem*
+        break
+    elif [[ $answer == "N" || $answer == "n" ]]; then
+        echo "Filesystem not cleared"
+        break
+    else
+        echo "Invalid input. Please enter Y or N."
+    fi
+done
